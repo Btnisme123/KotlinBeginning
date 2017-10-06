a12de8e434cf3c5874c01df6e921b99e7db0ccfb
Btnisme123 <vulan238@gmail.com>
2017-10-06 16:43:40 +0700

    request

--- app/src/main/java/com/example/framgianguyenvulan/kotlinbeginning/MainActivity.kt
+++ app/src/main/java/com/example/framgianguyenvulan/kotlinbeginning/MainActivity.kt
@@ -5,17 +5,30 @@ import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.example.framgianguyenvulan.kotlinbeginning.adapter.ForecastAdapter
+import com.example.framgianguyenvulan.kotlinbeginning.data.BaseTask
+import com.example.framgianguyenvulan.kotlinbeginning.data.Request
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
-
+    val url1 = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+            "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val forecastList = findViewById(R.id.forecast_list) as RecyclerView
         forecastList.layoutManager = LinearLayoutManager(this)
         forecastList.adapter = ForecastAdapter(items)
+
+        doAsync(){
+            Request(url1).run()
+            uiThread {  longToast("here")}
+        }
     }
 
+    val f1 =BaseTask(1,"1")
+    val(id, task)=f1
     private val items = listOf("Mon 6/23 - Sunny - 31/17",
             "Tue 6/24 - Foggy - 21/8",
             "Wed 6/25 - Cloudy - 22/17",
